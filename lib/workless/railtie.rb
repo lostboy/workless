5048853e0fe31f105360f9c38fc09d5435e89a81
3dd13 <tatonlto@gmail.com>
2011-03-13 23:14:26 +0800

    support delayed_job_mongoid as well. include the Scaler module to Mongoid:Job

--- lib/workless/railtie.rb
+++ lib/workless/railtie.rb
@@ -5,6 +5,7 @@ module Delayed
     initializer :after_initialize do
       Delayed::Worker.max_attempts = 3
       Delayed::Backend::ActiveRecord::Job.send(:include, Delayed::Workless::Scaler) if defined?(Delayed::Backend::ActiveRecord::Job)
+      Delayed::Backend::Mongoid::Job.send(:include, Delayed::Workless::Scaler) if defined?(Delayed::Backend::Mongoid::Job)      
     end
   end
 end
