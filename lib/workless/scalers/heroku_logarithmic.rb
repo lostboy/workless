7ec7b87dac6cccbe752ea638528d49cb1ad748d3
Benny Wong <benny@bwong.net>
2011-04-08 00:07:29 -0400

    Add logarithmic scaler

---
+++ lib/workless/scalers/heroku_logarithmic.rb
@@ -0,0 +1,37 @@
+require 'heroku'
+
+module Delayed
+  module Workless
+    module Scaler
+
+      class HerokuLogarithmic < Base
+
+        require "heroku"
+
+        def up
+          client.set_workers(ENV['APP_NAME'], num_workers) if workers == 0 and jobs.count > 0
+        end
+
+        def down
+          client.set_workers(ENV['APP_NAME'], 0) unless workers == 0 or jobs.count > 0
+        end
+
+        def workers
+          client.info(ENV['APP_NAME'])[:workers].to_i
+        end
+
+        private
+
+        def client
+          @client ||= ::Heroku::Client.new(ENV['HEROKU_USER'], ENV['HEROKU_PASSWORD'])
+        end
+
+        def num_workers
+          Math.log10(jobs.count).to_i
+        end
+
+      end
+
+    end
+  end
+end
