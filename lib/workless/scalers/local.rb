52390b437b030ab557c05bee6974ac92f5fbdefb
Jason Bellows <bellowsj@gmail.com>
2011-12-21 14:09:41 -0700

    modified local.rb so that the down method actually works on os x lion.

--- lib/workless/scalers/local.rb
+++ lib/workless/scalers/local.rb
@@ -12,7 +12,12 @@ module Delayed
         end
 
         def down
-          Rush::Box.new.processes.filter(:cmdline => /rake jobs:work/).kill unless workers == 0 or jobs.count > 0
+          #Rush::Box.new.processes.filter(:cmdline => /rake jobs:work/).kill unless workers == 0 or jobs.count > 0
+          #instead of just killing the process which exited the thing way too fast and didn't remove the entry from the database table
+          #now we will just set exit to true so that it will exit the loop in the run method in delayed_job
+          #and I used jobs.count - 1 because when the after_destroy hook is trigged the job is not yet actually destroyed.
+          #this works much better for my local development system, OS X Lion. 
+          $exit = true unless workers == 0 or jobs.count - 1 > 0
           true
         end
 
