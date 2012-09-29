4a6318cb176b662098e50a1ad92fa01ca7c0e630
Jaime Iniesta <jaimeiniesta@gmail.com>
2012-09-29 16:04:33 +0200

    reactivate pending spec

--- spec/workless/scaler_spec.rb
+++ spec/workless/scaler_spec.rb
@@ -23,7 +23,6 @@ describe Delayed::Workless::Scaler do
     end
 
     it 'should be the local scaler' do
-      pending "This spec passes alone, but fails when running the whole suite"
       Delayed::Job::Mock.scaler.should == Delayed::Workless::Scaler::Local
     end
 
