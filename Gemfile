cbc8304c90961b8d849ad8972406c97ed3a9dd22
David Westerink <davidakachaos@gmail.com>
2013-02-27 13:09:50 +0100

    Adding Coveralls support
    This is to see the spec/test coverage

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source :gemcutter
 
+gem 'coveralls', require: false
+
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ require 'bundler/setup'
 
 Bundler.require(:default)
 
+require 'coveralls'
+Coveralls.wear!
+
 require 'workless'
 
 module Delayed
