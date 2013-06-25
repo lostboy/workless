ec53e6fb3ab0f11aeb9df50eb83298730f0272d5
Jithu Gopal <jithug87@gmail.com>
2013-06-25 18:53:42 +0530

    Refactor a hard-to-read `unless/else` chain in scaler.rb

--- lib/workless/scaler.rb
+++ lib/workless/scaler.rb
@@ -9,17 +9,17 @@ module Delayed
 
       def self.included(base)
         base.send :extend, ClassMethods
-        unless base.to_s =~ /ActiveRecord/
-          base.class_eval do
-            after_destroy "self.class.scaler.down"
-            after_create "self.class.scaler.up"
-            after_update "self.class.scaler.down", :unless => Proc.new {|r| r.failed_at.nil? }
-          end
-        else
+        if base.to_s =~ /ActiveRecord/
           base.class_eval do
             after_commit "self.class.scaler.down", :on => :destroy
             after_commit "self.class.scaler.up", :on => :create
             after_commit "self.class.scaler.down", :on => :update, :unless => Proc.new {|r| r.failed_at.nil? }
+          end          
+        else
+          base.class_eval do
+            after_destroy "self.class.scaler.down"
+            after_create "self.class.scaler.up"
+            after_update "self.class.scaler.down", :unless => Proc.new {|r| r.failed_at.nil? }
           end
         end
 
